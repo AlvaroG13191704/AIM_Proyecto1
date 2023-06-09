eb6236a13bd58d067eb774efd64560457ea20e6f
Pablo Barahona <pbarahona07@gmail.com>
2023-06-08 20:51:40 -0600

    metodos create y delete cloud arreglados

--- app/cloud/createStorage.py
+++ app/cloud/createStorage.py
@@ -1,33 +1,20 @@
-import sys
 from google.oauth2 import service_account
 from google.cloud import storage
 
 google_credentials = service_account.Credentials.from_service_account_file("app/cloud/key.json")
 
-def create_cloud(file_data, destination_blob_name):
+def create_cloud(name, file_data, destination_blob_name):
   """Uploads file data to the bucket."""
   storage_client = storage.Client(credentials=google_credentials)
   bucket = storage_client.bucket("iam_project1_bucket")
-  blob = bucket.blob(destination_blob_name)
+  blob = bucket.blob(destination_blob_name + name)
 
   # CHECK IF THE FILE ALREADY EXISTS
   if blob.exists():
-    print(f"File {destination_blob_name} already exists.")
+    print(f"File {destination_blob_name + name} already exists.")
     return False
 
   blob.upload_from_string(file_data)
 
-  print(f"File data uploaded to {destination_blob_name}.")
-
-
-if __name__ == "__main__":
-  file_data = '''This is the content of the text file.
-  It can have multiple lines and contain any text data.
-  '''
-
-  create_cloud(
-      file_data=file_data,
-      destination_blob_name="ARCHIVOS/carpeta5/sub5/exampleSub1.txt",
-  )
-#  create_cloud(bucket_name="iam_project1_bucket", file_data=body, destination_blob_name=f"ARCHIVOS{path[:-1]}{name}")
+  print(f"File data uploaded to {destination_blob_name + name}.")
 
